7a5fe56bf2098f09ff4ed989faf8d99c87ff064a
Basarat Ali Syed <basaratali@gmail.com>
2015-04-27 19:01:00 +1000

    no message

--- errorcases/quickfix/stringConcatToTemplate/stringConcatToTemplate.js
+++ errorcases/quickfix/stringConcatToTemplate/stringConcatToTemplate.js
@@ -1,5 +1,5 @@
-var convertible1 = 'This is a part of a string concatenation' + 123456;
-var convertible2 = 'This is a part of a string concatenation' + 123456 + 'continuing';
-var convertible3 = 'This needs to be escaped : ` ' + 123456 + "Also escape ${honestly}";
+var convertible1 = 'This is a part of a string concatenation ' + 123456;
+var convertible2 = 'This is a part of a string concatenation ' + 123456 + ' continuing';
+var convertible3 = 'This needs to be escaped : ` ' + 123456 + " Also escape ${honestly}";
 var notConvertible = 'something already a string';
 var notConvertibleAsItsNumber = 1234356 + 123456789;
