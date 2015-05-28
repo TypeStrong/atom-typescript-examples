6c3b1a796156c800337375cb8d6c0757ff3fd7ec
Basarat Ali Syed <basaratali@gmail.com>
2015-05-28 18:13:24 +1000

    more concat tests

--- errorcases/quickfix/stringConcatToTemplate/stringConcatToTemplate.ts
+++ errorcases/quickfix/stringConcatToTemplate/stringConcatToTemplate.ts
@@ -1,6 +1,6 @@
 var convertible1 = 'This is a part of a string concatenation ' + 123456;
 var convertible2 = 'This is a part of a string concatenation ' + 123456 + ' continuing';
 var convertible3 = 'This needs to be escaped : ` ' + 123456 + " Also escape ${honestly}";
-var convertible4 = `template strings ${123} should be merged` + ' with strings ' + `by removing their backticks`;
+var convertible4 = `template strings ${123} should be merged` + ' with strings ' + `by removing their backticks num: ` + 456;
 var notConvertible = 'something already a string';
 var notConvertibleAsItsNumber = 1234356 + 123456789;
