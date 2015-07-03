6430cc16ba3dcbd0bfb388ab697f48beb996d3e4
Basarat Syed <basaratali@gmail.com>
2015-07-03 13:22:01 +1000

    htmltotsx

---
+++ tsx/htmltotsx/htmltotsx.js
@@ -0,0 +1,3 @@
+var foo =  < !--Hello, world = -- >
+    React.createElement("div", {"class": "awesome", "style": "border: 1px solid red"}, React.createElement("label", {"for": "name"}, "Enter your name: "), React.createElement("input", {"type": "text", "id": "name"}))
+    < p > Enter, your = HTML, here, p =  > ;
