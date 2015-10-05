b6dfbae5a25f04d5f38d67995e2a47808933b6ba
Basarat Syed <basaratali@gmail.com>
2015-10-05 16:09:27 +1100

    Format as a sample of https://github.com/TypeStrong/atom-typescript/issues/640 Try formatting this file on a mac and it increases indent consistently

--- grammartests/611/generic.ts
+++ grammartests/611/generic.ts
@@ -1,9 +1,9 @@
 class B<T, U> {
-   value: number = 2
-   str: Array<string> = ['abc','bbc']
+    value: number = 2
+    str: Array<string> = ['abc', 'bbc']
 }
 
-class C extends B<any , any> {
-  value: number = 5
-  str2: string = "10"
+class C extends B<any, any> {
+    value: number = 5
+    str2: string = "10"
 }
